2fdf85d1a1a71b332f97c075808c30ea555eaa41
Tr100000 <147051084+Tr100000@users.noreply.github.com>
2026-08-29 17:17:55 +0900

    fix

--- src/main.ts
+++ src/main.ts
@@ -3,6 +3,5 @@ import './assets/main.css';
 import { createApp } from 'vue';
 import App from './App.vue';
 import router from './router';
-import { createPinia } from 'pinia';
 
-createApp(App).use(createPinia()).use(router).mount('#app');
+createApp(App).use(router).mount('#app');
