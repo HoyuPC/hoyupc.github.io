93e2b6e5cf5e66d4cd51cf6cc84de5c82d8818cc
Tr100000 <147051084+Tr100000@users.noreply.github.com>
2026-09-17 11:31:17 +0900

    legacy

--- vite.config.ts
+++ vite.config.ts
@@ -2,10 +2,16 @@ import { fileURLToPath, URL } from 'node:url';
 
 import { defineConfig } from 'vite';
 import vue from '@vitejs/plugin-vue';
+import viteLegacyPlugin from '@vitejs/plugin-legacy';
 
 // https://vite.dev/config/
 export default defineConfig({
-  plugins: [vue()],
+  plugins: [
+    vue(),
+    viteLegacyPlugin({
+      targets: 'defaults, safari > 11',
+    }),
+  ],
   resolve: {
     alias: {
       '@': fileURLToPath(new URL('./src', import.meta.url)),
